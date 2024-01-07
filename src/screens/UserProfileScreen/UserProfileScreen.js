a9af4bf44a0c8a6f0372bc238f9f3a61a3f487d1
Andrei Joldea <andrei.joldea@student.upt.ro>
2024-01-08 00:44:17 +0200

    changes

--- src/screens/UserProfileScreen/UserProfileScreen.js
+++ src/screens/UserProfileScreen/UserProfileScreen.js
@@ -1,27 +1,29 @@
-import { serverIp, serverPort } from '../../network';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, useWindowDimensions, FlatList, Image, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import axios from "axios";
+import axios from 'axios';
+import { serverIp, serverPort } from '../../network';
 
 const UserProfileScreen = ({ route }) => {
     const { userId } = route.params;
     const { height } = useWindowDimensions();
     const navigation = useNavigation();
     const [detectedImages, setDetectedImages] = useState([]);
-
+    console.log({userId});
     useEffect(() => {
         const fetchDetectedImages = async () => {
-            try {
-                const response = await axios.get(`http://${serverIp}:${serverPort}/detection?userId=${userId}`);
-                setDetectedImages(response.data.detectedImages);
-            } catch (error) {
-                console.error('Error fetching detected images:', error);
-            }
+        try {   
+            const response = await axios.get(`http://${serverIp}:${serverPort}/detection?userId=${userId}`);
+            console.log('Detected Images Response:', response.data);
+            setDetectedImages(response.data.detectedImages);
+        } catch (error) {
+            console.error('Error fetching detected images:', error);
+        }
         };
     
         fetchDetectedImages();
     }, [userId]);
+    
 
     const handleImagePress = (imageDetails) => {
         navigation.navigate('ObjectDetection', {
@@ -29,22 +31,31 @@ const UserProfileScreen = ({ route }) => {
         });
     };
 
-    const renderDetectedImage = ({ item }) => (
-        <TouchableOpacity onPress={() => handleImagePress(item)}>
-        <Image source={{ uri: item.imageUrl }} style={styles.detectedImage} />
-        </TouchableOpacity>
-    );
+    const renderDetectedImage = ({ item }) => {
+        console.log('Image URL:', item.imageUrl);
+      
+        return (
+          <TouchableOpacity onPress={() => handleImagePress(item)}>
+            {item.imageUrl ? (
+              <Image source={{ uri: item.imageUrl }} style={styles.detectedImage} />
+            ) : (
+              <Text>No Image Available</Text>
+            )}
+          </TouchableOpacity>
+        );
+      };
+      
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Detected Images</Text>
-      <FlatList
-        data={detectedImages}
-        keyExtractor={(item) => item._id}
-        renderItem={renderDetectedImage}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+        <Text style={styles.header}>Detected Images</Text>
+        <FlatList
+            data={detectedImages}
+            keyExtractor={(item) => item._id}
+            renderItem={renderDetectedImage}
+        />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
