fa9365ff4430e31aa8940f827cbec28b5a04e523
andrei-lazarov <andrei.lazarov@student.upt.ro>
2024-01-10 03:21:39 +0200

    Delete all images function

--- src/screens/UserProfileScreen/UserProfileScreen.js
+++ src/screens/UserProfileScreen/UserProfileScreen.js
@@ -6,64 +6,84 @@ import { serverIp, serverPort } from '../../network';
 import * as FileSystem from 'expo-file-system';
 
 const UserProfileScreen = ({ route }) => {
-    const { userId } = route.params;
-    const navigation = useNavigation();
-    const [detectedImages, setDetectedImages] = useState([]);
-    console.log({userId});
-    useEffect(() => {
-        const fetchDetectedImages = async () => {
-        try {   
-            const response = await axios.get(`http://${serverIp}:${serverPort}/detection?userId=${userId}`);
-            console.log('Detected Images Response:', response.data);
-            setDetectedImages(response.data.detectedImages);
-        } catch (error) {
-            console.error('Error fetching detected images:', error);
-        }
-        };
-    
-        fetchDetectedImages();
-    }, [userId]);
-    
+  const { userId } = route.params;
+  const navigation = useNavigation();
+  const [detectedImages, setDetectedImages] = useState([]);
+  console.log({ userId });
 
-    const handleImagePress = (imageDetails) => {
-      const { userId, image, className, probability } = imageDetails;
-    
-      const detectionInfo = {
-        userId,
-        pickedImage: image,
-        prediction: [{ className, probability }],
-      };
-    
-      navigation.navigate('Object Detection', {
-        ...detectionInfo,
+  const fetchDetectedImages = async () => {
+    try {
+      const response = await axios.get(`http://${serverIp}:${serverPort}/detection?userId=${userId}`);
+      console.log('Detected Images Response:', response.data);
+      setDetectedImages(response.data.detectedImages);
+    } catch (error) {
+      console.error('Error fetching detected images:', error);
+    }
+  };
+
+  useEffect(() => {
+
+
+    fetchDetectedImages();
+  }, [userId]);
+
+  const deleteAllImages = async () => {
+    try {
+      const response = await axios.delete(`http://${serverIp}:${serverPort}/deleteAllImages`, {
+        data: { userId: userId },
       });
-    };
-    
 
-    const renderDetectedImage = ({ item }) => {
-      return (
-        <TouchableOpacity onPress={() => handleImagePress(item)}>
-          {item.image ? (
-            <Image source={{ uri: `data:image/jpeg;base64,${item.image}` }} style={styles.detectedImage} />
-          ) : (
-            <Text>No Image Available</Text>
-          )}
-        </TouchableOpacity>
-      );
-    };
-    
-      
+      console.log('Delete Images Response:', response.data);
+      // Optionally, you can fetch the updated list of images after deletion
+      await fetchDetectedImages();
+    } catch (error) {
+      console.error('Error deleting images:', error);
+    }
+  };
 
+
+  const handleImagePress = (imageDetails) => {
+    const { userId, image, className, probability } = imageDetails;
+
+    const detectionInfo = {
+      userId,
+      pickedImage: image,
+      prediction: [{ className, probability }],
+    };
+
+    navigation.navigate('Object Detection', {
+      ...detectionInfo,
+    });
+  };
+
+
+  const renderDetectedImage = ({ item }) => {
     return (
-        <View style={styles.container}>
-        <Text style={styles.header}>Detected Images</Text>
-        <FlatList
-            data={detectedImages}
-            keyExtractor={(item) => item._id}
-            renderItem={renderDetectedImage}
-        />
-        </View>
+      <TouchableOpacity onPress={() => handleImagePress(item)}>
+        {item.image ? (
+          <Image source={{ uri: `data:image/jpeg;base64,${item.image}` }} style={styles.detectedImage} />
+        ) : (
+          <Text>No Image Available</Text>
+        )}
+      </TouchableOpacity>
     );
+  };
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Detected Images</Text>
+      <TouchableOpacity onPress={deleteAllImages}>
+        <Text>Delete All Images</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={detectedImages}
+        keyExtractor={(item) => item._id}
+        renderItem={renderDetectedImage}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
