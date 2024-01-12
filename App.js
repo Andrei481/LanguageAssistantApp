545396960598391de2672e998f9d03e3b72579dd
andrei-lazarov <andrei.lazarov@student.upt.ro>
2024-01-12 16:22:42 +0200

    First .apk with no errors

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import 'react-native-gesture-handler';
 import Navigation from './src/navigation';
 
 export default function App() {
